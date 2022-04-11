09e22e935e5d7fa2260043e9ca751e050890ffbb
evgeni <evgeni.lilov88sliven@gmail.com>
2022-04-11 12:32:58 +0200

    clean up

--- vue.config.js
+++ vue.config.js
@@ -1,41 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
-const mix = require('laravel-mix');
 
 module.exports = defineConfig({
   publicPath: process.env.VUE_APP_BASE_URL,
   lintOnSave: true,
-  transpileDependencies: true,
-  // configureWebpack: {
-    // configureWebpack: config => {
-    //   config.entry = '@/wrapper/main.js'
-    //   config.pluginOptions =
-    // },
-  
-    devtool: 'source-map',
-    pluginOptions: {
-      browserExtension: {
-        components: {
-          background: true,
-          contentScripts: true
-        },
-        componentOptions: {
-          background: {
-            entry: './src/assets/background.js'
-          },
-          contentScripts: {
-            entries: {
-              'content-script': [
-                  './src/assets/contents.js'
-              ]
-            }
-          }
-        }
-      }
-    }
-  // },
-  
+  transpileDependencies: true 
 })
-
-mix.webpackConfig(webpack => { return {
-  plugins: [new webpack.DefinePlugin({ '__VUE_OPTIONS_API__': 'true', '__VUE_PROD_DEVTOOLS__': 'false' })],
-}});
