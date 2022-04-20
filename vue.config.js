ea484bcca021ddbafd95d860f59bfb1832af5a73
evgeni <evgeni.lilov88sliven@gmail.com>
2022-04-20 12:13:24 +0200

    cleanup1

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.VUE_APP_BASE_URL,
   lintOnSave: true,
-  transpileDependencies: true, //| ['vuex-persist']
+  transpileDependencies: true,
   configureWebpack: {
     entry: "./src/main.ts",
     experiments: {
